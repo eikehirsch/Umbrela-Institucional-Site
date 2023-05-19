4968b0a22df81be552b033ec34b98df749971497
eike.hirsch <eike.hirsch@penseavanti.com.br>
2023-05-19 10:30:32 -0300

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -5,31 +5,31 @@ import "./index.css";
 import App from "./App";
 import RaccoonCity from "./routes/raccoon-city/raccoonCity";
 import OurHistory from "./routes/our-history/ourHistory";
-import RedQueen from "./routes/red-queen/RedQueen"
+import RedQueen from "./routes/red-queen/RedQueen";
 import ErrorPage from "./routes/error-page/ErrorPage";
 
 // 1 - Setting router
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
   },
   {
     path: "/",
-    element: <App/>
+    element: <App />,
   },
   {
     path: "raccoon-city",
-    element: <RaccoonCity/>
+    element: <RaccoonCity />,
   },
   {
     path: "nossa-historia",
-    element: <OurHistory/>
+    element: <OurHistory />,
   },
   {
     path: "red-queen",
-    element: <RedQueen />
-  }
+    element: <RedQueen />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
